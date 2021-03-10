8de5e1a3418d1c0c824713210bb767ec95defde7
Yejin128 <bbaekhuen@gmail.com>
2021-03-10 17:11:40 +0900

    백준_단계별로 풀어보기_10818

--- 백준_단계별로 풀어보기_10818.c
+++ 백준_단계별로 풀어보기_10818.c
@@ -1,10 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int num[1000000] = {0,};
-	int num2;
-	
-	scanf("%d", &num2);
+int num[1000000] = { 0 };
 
+int main() {
+	int N; //입력받을 개수
+	int max = 0, min = 0; //최댓값 최솟값
+	scanf("%d", &N);
+	
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	max = num[0]; min = num[0]; //max와 min에 임의의 값 넣어주기
+	
+	for (int i = 0; i < N - 1; i++) {
+		if (max < num[i + 1]) {
+			max = num[i + 1];
+		}
+		if (min > num[i + 1]) {
+			min = num[i + 1];
+		}
+	}
+	printf("%d %d", max, min);
 }
